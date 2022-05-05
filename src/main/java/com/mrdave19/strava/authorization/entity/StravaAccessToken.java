280c15c956ede670990a688b68afd0c70fd3582c
Dawid Mazur <dawmaz@gmail.com>
2022-05-05 23:44:00 +0200

    Created entities. Create-drop hbm2ddl setting enabled.

--- src/main/java/com/mrdave19/strava/authorization/entity/StravaAccessToken.java
+++ src/main/java/com/mrdave19/strava/authorization/entity/StravaAccessToken.java
@@ -1,7 +1,65 @@
 package com.mrdave19.strava.authorization.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class StravaAccessToken {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name="athlete_id")
+    private long athleteId;
+
+    private String scope;
+
+    @Column(name="access_token")
+    private String accessToken;
+
+    @Column(name="expires_at")
+    private long expiresAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getAthleteId() {
+        return athleteId;
+    }
+
+    public void setAthleteId(long athleteId) {
+        this.athleteId = athleteId;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public long getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(long expiresAt) {
+        this.expiresAt = expiresAt;
+    }
 }
--- src/main/java/com/mrdave19/strava/authorization/entity/StravaRefreshToken.java
+++ src/main/java/com/mrdave19/strava/authorization/entity/StravaRefreshToken.java
@@ -1,8 +1,56 @@
 package com.mrdave19.strava.authorization.entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class StravaRefreshToken {
 
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name="athlete_id")
+    private long athleteId;
+
+    @Column(name="refresh_token")
+    private String refreshToken;
+
+    private String scope;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getAthleteId() {
+        return athleteId;
+    }
+
+    public void setAthleteId(long athleteId) {
+        this.athleteId = athleteId;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
 }
